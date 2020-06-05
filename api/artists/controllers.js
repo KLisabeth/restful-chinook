fe37d70ed814165e9e7285a9ee1a53cfed027609
KLisabeth <professional.translations4ppl@gmail.com>
2020-06-05 16:48:41 +0200

    artists

--- api/artists/controllers.js
+++ api/artists/controllers.js
@@ -14,14 +14,59 @@ const controllers = {
       res.json(rows)
     });
   },
-  getOne: (req, res) => { },
+  getOne: (req, res) => { 
+    const id = req.params.id;
+    const sql =`SELECT * FROM artists WHERE artistId = ${id}`;
+    db.all(sql, (err, rows) => {
+      if (err) {
+        res.status(400).json({ "error": err.message });
+        return;
+      }
+
+      res.json(rows)
+    });
+  },
   create: (req, res) => {
     // read row data from body
+    const newArtist = req.body;
+    const sql = `INSERT INTO artists (Name)
+    VALUES("${newArtist.Name}")`;
+    db.all(sql, (err, rows) => {
+      if (err) {
+        res.status(400).json({ "error": err.message });
+        return;
+      }
+
+      res.json("New Artist is Created")
+    });
   },
   update: (req, res) => {
     // read row data from body
+    const id = req.params.id;
+    const newArtist = req.body;
+    
+    const sql = `UPDATE artists SET Name = "${newArtist.Name}"
+    WHERE artistId = ${id}`;
+    db.all(sql, (err, rows) => {
+      if (err) {
+        res.status(400).json({ "error": err.message });
+        return;
+      }
+
+      res.json("Selected Artist is Updated")
+    });
   },
-  delete: (req, res) => { }
+  delete: (req, res) => {
+    const id = req.params.id;
+    const sql = `DELETE FROM artists WHERE ArtistId = ${id}`;
+    db.all(sql, (err, rows) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json("Selected Artist is Deleted");
+    });
+   }
 }
 
 module.exports = controllers;
