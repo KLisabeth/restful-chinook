03bcd0ee9ebc8907403f4b9cc27fefbf22c24eb9
KLisabeth <professional.translations4ppl@gmail.com>
2020-06-05 16:49:22 +0200

    playlists

--- api/playlists/controllers.js
+++ api/playlists/controllers.js
@@ -14,14 +14,59 @@ const controllers = {
       res.json(rows)
     });
   },
-  getOne: (req, res) => { },
+  getOne: (req, res) => { 
+    const id = req.params.id;
+    const sql =`SELECT * FROM playlists WHERE PlaylistId = ${id}`;
+    db.all(sql, (err, rows) => {
+      if (err) {
+        res.status(400).json({ "error": err.message });
+        return;
+      }
+
+      res.json(rows)
+    });
+  },
   create: (req, res) => {
     // read row data from body
+    const newPlaylist = req.body;
+    const sql = `INSERT INTO playlists (Name)
+    VALUES("${newPlaylist.Name}")`;
+    db.all(sql, (err, rows) => {
+      if (err) {
+        res.status(400).json({ "error": err.message });
+        return;
+      }
+
+      res.json("New Playlist is Created")
+    });
   },
   update: (req, res) => {
     // read row data from body
+    const id = req.params.id;
+    const newPlaylist = req.body;
+    
+    const sql = `UPDATE playlists SET Name = "${newPlaylist.Name}"
+    WHERE PlaylistId = ${id}`;
+    db.all(sql, (err, rows) => {
+      if (err) {
+        res.status(400).json({ "error": err.message });
+        return;
+      }
+
+      res.json("Selected Playlist is Updated")
+    });
   },
-  delete: (req, res) => { }
+  delete: (req, res) => { 
+    const id = req.params.id;
+    const sql = `DELETE FROM playlists WHERE PlaylistId = ${id}`;
+    db.all(sql, (err, rows) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json("Selected Playlist is Deleted");
+    });
+  }
 }
 
 module.exports = controllers;
