09387c7b9d0a60fab62cfa669cb29b33b109090d
KLisabeth <professional.translations4ppl@gmail.com>
2020-06-05 16:48:53 +0200

    genres

--- api/genres/controllers.js
+++ api/genres/controllers.js
@@ -14,14 +14,59 @@ const controllers = {
       res.json(rows)
     });
   },
-  getOne: (req, res) => { },
+  getOne: (req, res) => { 
+    const id = req.params.id
+    const sql = `SELECT * FROM genres WHERE GenreId = ${id}`;
+    db.all(sql, (err, rows) => {
+      if (err) {
+        res.status(400).json({ "error": err.message });
+        return;
+      }
+
+      res.json(rows)
+    });
+  },
   create: (req, res) => {
     // read row data from body
+    const newGenre = req.body;
+    const sql = `INSERT INTO genres (Name)
+    VALUES("${newGenre.Name}")`;
+    db.all(sql, (err, rows) => {
+      if (err) {
+        res.status(400).json({ "error": err.message });
+        return;
+      }
+
+      res.json("New Genre is Created")
+    });
   },
   update: (req, res) => {
     // read row data from body
+    const id = req.params.id;
+    const newGenre = req.body;
+    
+    const sql = `UPDATE genres SET Name = "${newGenre.Name}"
+    WHERE GenreId = ${id}`;
+    db.all(sql, (err, rows) => {
+      if (err) {
+        res.status(400).json({ "error": err.message });
+        return;
+      }
+
+      res.json("Selected Genre is Updated")
+    });
   },
-  delete: (req, res) => { }
+  delete: (req, res) => {
+    const id = req.params.id;
+    const sql = `DELETE FROM genres WHERE GenreId = ${id}`;
+    db.all(sql, (err, rows) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json("Selected Genre is Deleted");
+    });
+   }
 }
 
 module.exports = controllers;
